ac8967d826b660209eef4b422b43042ce4c21213
badissaidane <saidanebadis1920@gmail.com>
2024-11-02 02:56:29 +0100

    inbording page nearly done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
-
-  return (
-    <div>
-     
-    </div>
-  )
+    return (
+        <>
+            <div className='welcomeContainer'>
+                <p className='bienVenue'>Bienvenue au Resto Ali</p>
+                <button className='startButton'>Commencez</button> {/* Added a class for potential styling */}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
