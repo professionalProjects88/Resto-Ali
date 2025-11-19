602a759c947fe29c3bbf4f1a18230ca8bf4b3fdc
badissaidane <saidanebadis1920@gmail.com>
2025-11-19 18:55:47 +0100

    test

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ app.use(cors());
 app.use("/api/food", food);
 app.use("/api/user", user);
 
+app.get('/api', (req, res) => res.send('API is running'));
+
+// Serve React frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
+
 
 const port=process.env.PORT
 app.listen(port, () => {
